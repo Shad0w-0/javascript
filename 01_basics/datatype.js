c12ed7338eb7f3ff6ccda987d21f672a5529c74c
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2024-06-16 06:36:40 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") we are using nodejs and browrser
+
+console.log(3+3);  //maintain code readability 
+console.log("sahil");
+
+let name = "sahil" //string
+let age = 20 // variable
+let islogin =false //boolean
+let state; //undefined
+
+//number => 2 to pwr 53
+//bigint for very large values
+//string => ""
+//boolean => True/false
+//null =>standalone value
+//undefined => when its not defined yet eg above
+//symbol => unique to check the uniqueness
+
+// object
+console.log(typeof age); //typeof showed to show the type of the variable
+console.log(typeof undefined);//type undefined
+console.log(typeof null);// type object
