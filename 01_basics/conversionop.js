3c2a19b0e43a179c6c295327df06efd00799a808
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2024-06-16 07:28:46 +0000

    operations

--- 01_basics/conversionop.js
+++ 01_basics/conversionop.js
@@ -13,7 +13,46 @@ let valuenum = Boolean(score)
 // "" =>false
 // "sahil" =>true
 
-let num= 0
-let connem = Boolean(num)
-console.log(connem);
-console.log(typeof connem);
+// let num= 0
+// let connem = Boolean(num)
+// console.log(connem);
+// console.log(typeof connem);
+
+
+//*******************OPERATIONS*****************
+
+let value = 3
+let negvalue= -value
+//console.log(negvalue);
+
+//basics operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// string concatenation
+// let str1 = "hello"
+// let str2 = " world"
+// let str3 = str1 + str2
+// console.log(str3);
+
+//console.log("1" + 2); op is 12 as first value string
+//console.log(1 + "2"); op is 12 as 2nd value string
+//console.log("2" + 3 + 4);//op is 234 bcz the oen value is string so it takes all the others values in front of it as string
+//console.log(2+3+"4"); //op is 54 bcz the first 2 num is var so they got added and the next value of string which got concatenated
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+// console.log(false);
+// console.log(+false);
+
+
+//increment value
+let gamecounter = 100
+gamecounter--
+console.log(gamecounter);
