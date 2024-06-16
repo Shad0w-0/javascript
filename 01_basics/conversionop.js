8e2910f73f409351cdc590f85e20c2382bc002f9
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2024-06-16 07:01:08 +0000

    conversion of datatype

---
+++ 01_basics/conversionop.js
@@ -0,0 +1,19 @@
+let score= "sahil"
+//console.log(typeof score);
+
+let valuenum = Boolean(score)
+//console.log(valuenum);
+
+
+
+//conversions
+// "33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
+// "" =>false
+// "sahil" =>true
+
+let num= 0
+let connem = Boolean(num)
+console.log(connem);
+console.log(typeof connem);
