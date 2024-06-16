f3fa25297aae7bc858db3a9b457cabdd1469b02f
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2024-06-16 07:43:51 +0000

    campare done

---
+++ 01_basics/compare.js
@@ -0,0 +1,19 @@
+//compare
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+//diff data types
+// console.log("2">1);
+// console.log(2 > "1");
+
+//null comparision (can be confusing)
+// console.log(null > 0);//false
+// console.log(null < 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);// True bcz in this case == and comparison work differently >< >= <= comparison convert null value to number treating it as 0
+// console.log(undefined == 0);//false
+
+//=== strict check
+console.log("2"===2);//false bcz strict check not only checks the value but also checks the datatype
