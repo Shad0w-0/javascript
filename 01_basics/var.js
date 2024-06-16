fcd9b6d4806e4fd11cc3e22a1902689727f9d68b
sahil shetker <68986333+Shad0w-0@users.noreply.github.com>
2024-06-16 06:16:05 +0000

    var basic

---
+++ 01_basics/var.js
@@ -0,0 +1,12 @@
+const accountID = 144
+let accountemail = "sahilshetker4@gmail.com"
+var accountpass = "123"
+accountCity = "Jaipur"
+let accountstate;
+
+//accountID = 3 not allowed
+accountemail= "utkarsh"
+accountpass= "000"
+accountCity="goa"
+//console.log(accountpass);
+console.table([accountID,accountemail,accountpass,accountCity,accountstate])
