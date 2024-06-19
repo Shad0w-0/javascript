da924e186ea1f5fd5384243c8928870878cdffc5
sahil shetker <68986333+sahilshetker@users.noreply.github.com>
2024-06-19 06:36:01 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+//switch syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;//breal the control flow
+    case "aparil":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default");
+        break;
+}
