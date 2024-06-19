d2aa44b14b5be843b85ebc95b9a8c8d81af2c27e
sahil shetker <68986333+sahilshetker@users.noreply.github.com>
2024-06-19 07:32:38 +0000

    truth

---
+++ 04_control_flow/truth.js
@@ -0,0 +1,49 @@
+//when u assue the string is ture value
+//u dont give any condition in this case
+
+// const email=[]
+// if (email)
+//     {
+//         console.log("user logged in");
+//     }
+//     else{
+//         console.log("user is not logged in");
+//     }
+
+//     //if the string is empty prints not logged in
+
+//     //points to be notes
+//     //falsy values-false, 0, -0, BigInt 0n,"",null,undefined,NaN
+//     //truthy value- true , "0",'false'," ",[],{}, function(){}
+
+
+//     // if (email.length ===0)
+//     //     {
+//     //         console.log("Array is empty");
+//     //     }   
+
+//     const emptyobj={}
+//     if (Object.keys(emptyobj).length ===0)
+//         {
+//             console.log("object is empty");
+//         }   
+
+
+        //Nullish coalescing Operator(??): null undefined
+        //if null value come it checks for safety
+
+        // let val1=5 ?? 10 op-5
+        // let val1=null ?? 10 op-10 as the first is null
+        //  let val1=undefined ?? 10  op-10 same reason
+
+        // let val1= null??20??10//it return the first value after the ??
+        // console.log(val1);
+
+
+//Terniary oprator
+//syntax- condition ? true: false
+//example
+
+// const icecreamprice=100
+// icecreamprice<80 ? console.log("orice less then 80"):console.log("more then 80");
+
