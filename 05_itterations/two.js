81da2b0f8f39b563fce48d1e5a2eaa9a11f5f190
sahil shetker <68986333+sahilshetker@users.noreply.github.com>
2024-06-20 06:27:54 +0000

    while and do_while loops

---
+++ 05_itterations/two.js
@@ -0,0 +1,26 @@
+//while loop
+let i=0
+while(i <=10)
+    {
+       // console.log(`value of index is ${i}`);
+        i=i+2
+    }
+
+    //with array
+    let myarr=["flash","batman","superman"]
+    arr=0
+    while (arr<myarr.length) {
+        //console.log(`value is ${myarr[arr]}`);
+        arr=arr+1
+    }
+
+
+//do-while loop it executes first and then checks for the condition
+
+let score =11
+
+do {
+   // console.log(`value is ${score}`);
+    score++
+} while (score<10);
+
