ec0d8fd84b07be6eac38cc64ac6519fead5bd4d0
sahil shetker <68986333+sahilshetker@users.noreply.github.com>
2024-06-20 14:34:53 +0000

    filters

--- 05_itterations/six.js
+++ 05_itterations/six.js
@@ -19,11 +19,62 @@ const Mynum=[1,2,3,4,5,6,7,8,9,10]
 
 //can be done other way also but there you need to put some logic
 //using foreach and a conditional if statement
-const newnum=[]
-Mynum.forEach((num)=> {
-    if(num>5)
-        {
-            newnum.push(num)
-        }
-})
-console.log(newnum);
+// const newnum=[]
+// Mynum.forEach((num)=> {
+//     if(num>5)
+//         {
+//             newnum.push(num)
+//         }
+// })
+// console.log(newnum);
+
+const books = [
+    {title: 'BOOK one',genre:'Friction',publish:1981,edition: 2004},
+    {title: 'BOOK two',genre:'Non-Friction',publish:1992,edition: 2008},
+    {title: 'BOOK three',genre:'history',publish:1999,edition: 2007},
+    {title: 'BOOK four',genre:'Non-Friction',publish:1989,edition: 2010},
+    {title: 'BOOK five',genre:'science',publish:2009,edition: 2014},
+    {title: 'BOOK six',genre:'Friction',publish:1987,edition: 2010},
+    {title: 'BOOK seven',genre:'history',publish:1986,edition: 1996},
+    {title: 'BOOK eight',genre:'Science',publish:2011,edition: 2016},
+];
+
+// const userBooks= books.filter((bk) => bk.genre==='history')
+
+// console.log(userBooks);
+
+// op
+// [
+//     {
+//       title: 'BOOK three',
+//       genre: 'history',
+//       publish: 1999,
+//       edition: 2007
+//     },
+//     {
+//       title: 'BOOK seven',
+//       genre: 'history',
+//       publish: 1986,
+//       edition: 1996
+//     }
+//   ]
+const userBooks= books.filter((bk) =>  bk.publish >=1995 || bk.genre==="history")
+console.log(userBooks);
+
+// op
+// [
+//     {
+//       title: 'BOOK five',
+//       genre: 'science',
+//       publish: 2009,
+//       edition: 2014
+//     },
+//     {
+//       title: 'BOOK eight',
+//       genre: 'Science',
+//       publish: 2011,
+//       edition: 2016
+//     }
+//   ]
+
+//can also use && operatin or ||
